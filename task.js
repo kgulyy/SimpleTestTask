29c63212ef35afc6f54b2c793313dfa8431ed567
Konstantin Gulyy <kgulyy@my.com>
2017-12-04 17:21:26 +0300

    Found better solution

--- task.js
+++ task.js
@@ -1,54 +1,23 @@
 /**
- * Внутренняя сортировка каждого элемента списка
- * @param {string[]} list
- * @returns {string[]}
- */
-function innerSort(list) {
-    const result = [];
-    list.forEach((value, index) => {
-        // разбить строку на буквы, отсортировать и слить обратно
-        const sorted = list[index].split('').sort().join('');
-        result.push(sorted);
-    });
-
-    return result;
-}
-
-/**
- * Поиск индексов дубликатов в списке
- * @param {string[]} list
- * @returns {Array<[int, int]>}
- */
-function getDuplicatesIndex(list) {
-    const result = {};
-    for (let i = 0; i < list.length; i++) {
-        if (result.hasOwnProperty(list[i])) {
-            result[list[i]].push(i);
-        } else if (list.lastIndexOf(list[i]) !== i) {
-            result[list[i]] = [i];
-        }
-    }
-    return Object.values(result);
-}
-
-/**
  * Получения двумерный массив анаграмм из произвольного массива слов
  * @param   {string[]} list
  * @returns {Array<[string, string]>}
  */
 function getAnagrams(list) {
-    const result = [];
+    const anagrams = {};
 
-    const innerSortedList = innerSort(list);
-    const duplicatesIndex = getDuplicatesIndex(innerSortedList);
-
-    duplicatesIndex.forEach(array => {
-        const resultSubArray = [];
-        array.forEach(index => resultSubArray.push(list[index]));
-        result.push(resultSubArray);
+    list.forEach(value => {
+        const sorted = value.split('').sort().join('');
+        if (anagrams[sorted] != null) {
+            anagrams[sorted].push(value);
+        } else {
+            anagrams[sorted] = [value];
+        }
     });
 
-    return result;
+    const result = Object.values(anagrams);
+
+    return result.filter(value => value.length > 1);
 }
 
 const inputList = [
